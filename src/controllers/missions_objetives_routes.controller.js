4c6e2b786b9ad4a1a1bc7d9af27a67e0e4dbe1a6
Amstrong <monachelloa@uninorte.edu.co>
2022-03-10 19:50:43 -0500

    feat: routes

--- src/controllers/missions_objetives_routes.controller.js
+++ src/controllers/missions_objetives_routes.controller.js
@@ -1,7 +1,7 @@
 const controller = {};
 const missionObjetivesModel = require("../models/missionsObjetives");
 
-//Create Image
+//Create MissionObjetives
 controller.createMissionObjetive = async (req, res) => {
   const { id_mission_objetive, name, description, count, mission } = req.body;
   const idImage = await missionObjetivesModel.findOne({
@@ -26,20 +26,51 @@ controller.createMissionObjetive = async (req, res) => {
   }
 };
 
-//getAllimages
+//getAllMissionsObjetives
 controller.getAllMissionObjetive = async (req, res) => {
   const images = await missionObjetivesModel.find();
   res.json({ images });
 };
 
-//getImage
+//getMissionsObjetive
 controller.getMissionObjetive = async (req, res) => {
   const b = req.params.id_mission_objetive;
-  const image = await missionObjetivesModel.find({ _id: b });
-  res.json({ image });
+  const mission = await missionObjetivesModel.find({ _id: b });
+  res.json({ mission });
 };
 
-//deleteImage
+//UpdateMissionsObjetives
+controller.updateMissionsObjetive = async (req, res) => {
+  const { id_mission_objetive, name, description, count } = req.body;
+  const filter = { _id: req.params.id_mission_objetive };
+  const update = { id_mission_objetive, name, description, count };
+  if (
+    (id_mission_objetive =
+      !undefined &&
+      name != undefined &&
+      description != undefined &&
+      count != undefined)
+  ) {
+    try {
+      const idUser = await missionObjetivesModel.findOneAndUpdate(
+        filter,
+        update,
+        { new: true }
+      );
+      if (idUser != undefined) {
+        res.json("updated");
+      } else {
+        res.json("missing id");
+      }
+    } catch (error) {
+      res.json("error.", error);
+    }
+  } else {
+    res.json("data missing");
+  }
+};
+
+//deleteMissionObjetive
 controller.deleteMissionObjetive = async (req, res) => {
   const filter = { _id: req.params.id_mission_objetive };
   try {
---
+++ src/routes/image_2d.js
@@ -0,0 +1,26 @@
+const { Router } = require('express');
+const router = Router();
+const {createImage, getAllImages, getImage, deleteImage} = require('../controllers/images_2d_routes.controller')
+
+router.route('/')
+      .get((req,res) => {
+            res.json(
+            {message: 'Bep, Bop! I am an API developed by Jaime Sierra'})})
+
+//CREATE
+router.route('/newImage/')
+      .post(createImage)
+
+//READ
+router.route('/getAllImages')
+      .get(getAllImages)
+
+router.route('/getImages/:id_image')
+      .get(getImage)
+
+
+//DELETE
+router.route('/deleteImages/:id_image')
+      .delete(deleteImage)
+
+module.exports = router;
